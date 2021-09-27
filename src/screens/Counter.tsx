1a07836d62a5c153c31415e729977702575f9cf6
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-09-27 13:31:38 +0700

    :hammer: fix types

--- src/screens/Counter.tsx
+++ src/screens/Counter.tsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { connect, ConnectedProps } from 'react-redux';
 
 import Button from '../components/Button';
 import SizedBox from '../components/SizedBox';
 import { increaseCounter, decreaseCounter } from '../store/actions/UiCounter';
 
-class Counter extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.box}>
-          <Text style={styles.textCounter}>{this.props.counter}</Text>
-          <View style={styles.buttonView}>
-            <Button title='- Decrease' onPress={() => {
-              this.props.decreaseCounter();
-            }} />
-            <SizedBox width={10} />
-            <Button title='+ Increase' onPress={() => {
-              this.props.increaseCounter();
-            }} />
-          </View>
+interface RootState {
+  counterConfigure: {
+    counter: 0
+  }
+}
+
+const Counter = (props: Props) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.textCounter}>{props.counter}</Text>
+        <View style={styles.buttonView}>
+          <Button title='- Decrease' onPress={() => {
+            props.setDecrease();
+          }} />
+          <SizedBox width={10} />
+          <Button title='+ Increase' onPress={() => {
+            props.setIncrease();
+          }} />
         </View>
       </View>
-    );
-  };
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -46,17 +51,17 @@ const styles = StyleSheet.create({
   }
 })
 
-function mapStateToProps(state) {
-  return {
-    counter: state.counterConfigure.counter
-  }
-}
+const mapStateToProps = (state: RootState) => ({
+  counter: state.counterConfigure.counter
+});
 
-function mapDispatchToProps(dispatch) {
-  return {
-    increaseCounter: () => dispatch(increaseCounter()),
-    decreaseCounter: () => dispatch(decreaseCounter())
-  }
-}
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  setIncrease: () => dispatch(increaseCounter()),
+  setDecrease: () => dispatch(decreaseCounter())
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+const connector = connect(mapStateToProps, mapDispatchToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type Props = PropsFromRedux;
+
+export default connector(Counter);
