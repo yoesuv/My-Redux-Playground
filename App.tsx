76d64372cb5a0b6671cd4c8e15a6ca1e2c3f2ac3
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-20 16:28:55 +0700

    :racehorse: optimize options

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator, NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux';
 
 import Splash from './src/screens/Splash';
@@ -15,52 +15,27 @@ import store from './src/redux/store'
 const configureStore = store();
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
+const baseOptions: NativeStackNavigationOptions = {
+  title: "My Redux Playground",
+  headerStyle: {
+    backgroundColor: THEME_COLOR
+  },
+  headerTintColor: 'white',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  }
+};
+
 export default function App() {
   return (
     <Provider store={configureStore}>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
-          <Stack.Screen name="Home" component={Home} options={{
-            title: 'My Redux Playground',
-            headerStyle: {
-              backgroundColor: THEME_COLOR
-            },
-            headerTintColor: 'white',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            }
-          }} />
-          <Stack.Screen name="Counter" component={Counter} options={{
-            title: 'Counter',
-            headerStyle: {
-              backgroundColor: THEME_COLOR
-            },
-            headerTintColor: 'white',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            }
-          }} />
-          <Stack.Screen name="FormArguments" component={FormAndArguments} options={{
-            title: 'Form & Arguments',
-            headerStyle: {
-              backgroundColor: THEME_COLOR
-            },
-            headerTintColor: 'white',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            }
-          }}/>
-          <Stack.Screen name="ListPlace" component={ListPlace} options={{
-            title: 'Request Api',
-            headerStyle: {
-              backgroundColor: THEME_COLOR
-            },
-            headerTintColor: 'white',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            }
-          }} />
+          <Stack.Screen name="Home" component={Home} options={{...baseOptions, title: "My Redux Playground"}} />
+          <Stack.Screen name="Counter" component={Counter} options={{...baseOptions, title: "Counter"}} />
+          <Stack.Screen name="FormArguments" component={FormAndArguments} options={{...baseOptions, title: "Form & Arguments"}}/>
+          <Stack.Screen name="ListPlace" component={ListPlace} options={{...baseOptions, title: "Networking"}} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
