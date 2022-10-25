dcd534f0d6896f238d936b58b4bf7fc48d0a43a7
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-25 08:58:24 +0700

    :pencil: update list place type

--- src/redux/types/list-place-type.tsx
+++ src/redux/types/list-place-type.tsx
@@ -1,12 +1,16 @@
+import { PlaceModel } from "../../models/place-model";
+
 export const LIST_PLACE_LOADING_START = "LIST_PLACE_LOADING_START";
 export const LIST_PLACE_LOADING_STOP = "LIST_PLACE_LOADING_STOP";
 
 interface ListPlaceLoadingStart {
     type: typeof LIST_PLACE_LOADING_START;
+    places: PlaceModel[],
 }
 
 interface ListPlaceLoadingStop {
     type: typeof LIST_PLACE_LOADING_STOP;
+    places: PlaceModel[],
 }
 
 export type ListPlaceActionType = ListPlaceLoadingStart | ListPlaceLoadingStop;
