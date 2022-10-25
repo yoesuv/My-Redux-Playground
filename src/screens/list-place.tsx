2634f924caade09b698c977ac8cfa3d18a7f6bf0
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-25 10:07:44 +0700

    :sparkles: connect react query

--- src/screens/list-place.tsx
+++ src/screens/list-place.tsx
@@ -1,14 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { useEffect } from 'react';
 import { StyleSheet, SafeAreaView, Text, View, ActivityIndicator } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { listPlaceLoadingStart, listPlaceLoadingStop } from '../redux/actions';
 import { RootState } from '../redux/reducers';
-
+import UseList from '../services/list-place-service';
 
 export default function ListPlace() {
 
   const dispatch = useDispatch();
   const state = useSelector((state: RootState) => state.listPlace);
+  const queryClient = new QueryClient();
 
   useEffect(() => {
     dispatch(listPlaceLoadingStart());
@@ -18,21 +20,36 @@ export default function ListPlace() {
   }, []);
 
   return (
-    <SafeAreaView style={styles.container}>
-      {state.isLoading ? <Loading /> : <BuildContent />}
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView style={styles.container}>
+        <BuildContent />
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 
   function Loading(): JSX.Element {
-    return <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center'}}>
+    return <View style={styles.containerCenter}>
       <ActivityIndicator size='large' />
     </View>
   }
 
-  function BuildContent(): JSX.Element {
-    return <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center'}}>
-      <Text style={styles.label}>Data Count : 10</Text>
+  function Error(): JSX.Element {
+    return <View style={styles.containerCenter}>
+      <Text style={styles.label}>Opss...</Text>
     </View>
+  }
+
+  function BuildContent(): JSX.Element {
+    const { data, status } = UseList();
+    if (status === 'loading') {
+      return <Loading />
+    }
+    if (status === 'success') {
+      return <View style={styles.containerCenter}>
+        <Text style={styles.label}>Data Count : {data.length}</Text>
+      </View>
+    }
+    return <Error />
   }
 
 };
@@ -46,5 +63,10 @@ const styles = StyleSheet.create({
     fontSize: 33,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  containerCenter: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
   }
 });
--- src/services/list-place-service.tsx
+++ src/services/list-place-service.tsx
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
+import { PlaceModel } from '../models/place-model';
 
 const fetchList = async () => {
   const result = await fetch('https://info-malang-batu.firebaseapp.com/List_place_malang_batu.json');
   return result.json();
 }
 
-const UseList = () => useQuery(['listPlace'], fetchList);
+const UseList = () => useQuery<PlaceModel[], Error>(['listPlace'], fetchList);
 
 export default UseList;
